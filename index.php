ab03844f605ae439b7ed2c0ee10608ed74d9a69f
Aditi009 <55443683+Aditi009@users.noreply.github.com>
2020-01-30 21:57:23 +0530

    Add files via upload

--- index.php
+++ index.php
@@ -3,34 +3,49 @@
 		<link href='https://fonts.googleapis.com/css?family=Eagle Lake' rel='stylesheet'>
 		<link rel="stylesheet" type="text/css" href="login.css" />
 	</head>
-	<body>
+	<body><center>
+		<div id="part1">
+		<header id="head">
+			<div id="list"><ul>
+			<li class="active"><a href="#">home</a></li>
+			<li><a href="#" onclick="abc2()">sign-in</a></li>
+			<li ><a href="#"  onclick="abc2()" ondblclick="abc3()" >login</a></li>
+				<li ><a href="#">client</a></li>
+				<li ><a href="#">Service</a></li></ul>
+				</div>
+		</header>
 		
+
 		
-		
-			
+			<div class="front">
 				<form action="dblogin.php" method="post" enctype="multipart/form-data">
-					<center>
-						<div class="front">
+				
+						
 				<span>Roll No</span>
 				<br><input type="number" name="roll_no" required>
+					<br>
 				<br>
 				<span>Email</span>
 				<br><input type="email" name="email" required >
 				
 					<br>
 					<br>
-					
+					<br>
 									<button id="but2"  
 									onmouseover="on2()" onclick="on()" 	>	Select Cover Photo</button>
 							<br>
 							<br>
-						<input type="file" name="image" required="required"/>
-		<input type="submit" name="submit" value="upload"/>
-							
 					
-					</div>
+							
+		<input id="btn2" type="submit" name="submit" value="upload"/>
+							
+				
+							
+				
 					
 						
+						
+		
 					<div id="cardimg">
 						<img id="img" src="user.png">
 						<input type="file" name="img1" id="inp" required="required"/>
@@ -39,11 +54,19 @@
 						<br>
 					<button><img id="img2" src="back.png" onclick="im()"></button>	
 				</div>
-						</center>
 					</form>
 				
-				
+		</div>
 		
+		
+		</div>
+		<div id="part2">
+		
+			<div id="main_div"></div>
+	</div>
+			
+
+			
 	<script>
 		function im()
 		{
@@ -67,6 +90,16 @@
 				 
 			
 		}
+		function abc2()
+		{
+			
+			document.querySelector(".front").style.display="block";
+		}
+			function abc3()
+		{
+			
+			document.querySelector(".front").style.display="none";
+		}
 		
 		
 		function abc()
@@ -80,4 +113,5 @@
 		{document.getElementById("but").style.marginTop="-2px";
 		 
 		}
-		</script></body></html>
+		</script>
+	</center></body></html>
